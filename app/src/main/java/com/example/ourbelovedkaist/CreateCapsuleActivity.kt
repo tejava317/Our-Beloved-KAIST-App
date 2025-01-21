e98ddf38528c49c3e7ba69bf43dd06276ccf46d8
tejava317 <frogjj@kaist.ac.kr>
2025-01-21 18:41:25 +0900

    feat: Implement main screen (buttons)

---
+++ app/src/main/java/com/example/ourbelovedkaist/CreateCapsuleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ourbelovedkaist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateCapsuleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_capsule)
+    }
+}
